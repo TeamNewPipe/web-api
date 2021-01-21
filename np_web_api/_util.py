1dafdce4efe9ccead5264702ec935c631ec2d94d
TheAssassin <theassassin@assassinate-you.net>
2021-01-21 22:31:36 +0100

    Accept only 200 OK responses from APIs
    Was done like that before. The easiest way to recognize issues with APIs (e.g., rate limits). Makes sure they're logged properly.

--- np_web_api/_util.py
+++ np_web_api/_util.py
@@ -8,4 +8,6 @@ async def fetch_text(url: str):
 
     async with aiohttp.ClientSession(headers=headers) as session:
         async with session.get(url) as response:
+            response.raise_for_status()
+
             return await response.text()
