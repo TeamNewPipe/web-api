a5410ff3d3f546d5f66489a95b9442f86eed6dfe
TheAssassin <theassassin@assassinate-you.net>
2021-02-06 02:03:10 +0100

    Fix cache-deletes-data-prematurely issue
    This commit fixes a _very_ awkward to debug issue which could not be reproduced for many days in the development environment, but occurred all the time in production. The cached data was basically just gone for some reason after 5 minutes, and there was no explanation available nor logs that would've explained the behavior.
    
    Flask-Caching thinks it's a good idea to hardcode a default timeout for the cached data. Our application requires that data is stored at least until the error timeout has triggered (1h) to allow one request handler to update it with new data.
    For some reason, the author does not want to introduce a possibility to disable timeouts by default, or to set timeout to infinity. Therefore, we just set it to some absurdly high number (2^31 at this point, which equals ~68 years) to achieve basically the same goal.
    
    Note for self: Python 3 no longer offers sys.maxint, and no, sys.maxsize is _not_ the same thing.

--- np_web_api/__init__.py
+++ np_web_api/__init__.py
@@ -3,7 +3,18 @@ from quart import Blueprint
 
 # using the app factory pattern, which means these are imported/initialized in a factory function
 bp = Blueprint(__name__, "bp")
-cache = Cache(with_jinja2_ext=False, config={"CACHE_TYPE": "simple"})
+
+
+# initialize cache, making sure that the values won't expire in a practical amount of time
+# using an absurdly high value is a bad solution, and the extension should just support that, but the author
+# indicated that they don't want to do that
+# see https://github.com/sh4nks/flask-caching/issues/183 for more information
+# simple cache means in-memory, which avoids unnecessary disk I/O and simplifies debugging
+# also, it works well when synchronizing the requests as we do to avoid third-party API rate limits
+cache = Cache(with_jinja2_ext=False, config={
+    "CACHE_TYPE": "simple",
+    "CACHE_DEFAULT_TIMEOUT": 2**31,
+})
 
 
 # must import these after creating bp/cache
